Think I found a bug in the way gradient descent was performed. Forget to additionally subract the vertical shift from resultant simulated values. Not an issue in MPC approach because the cost functions ensure they track.

alphaOnly_estimateParams.py
@@ -69,7 +69,9 @@ if __name__=="__main__":
     n = len(CI_Meas)
 
     # apply sigmoidal filter to help minimize noise
-    CI_Meas = sigmoid(CI_Meas) - 0.15
+    vs = 0.15
+    CI_Meas = sigmoid(CI_Meas) - vs
+    #if dset == 
 
     # set up timevec, recordings were resampled to 100 hz
     imRate = 1/100
@@ -101,7 +103,7 @@ if __name__=="__main__":
     alpha_list = np.arange(3, 40, 15)
     print(alpha_list)
 
-    gamma = 10 #dsets  6, ... require a higher value of gamma or alpha will go negative. 
+    gamma = 1 #dsets  6, ... require a higher value of gamma or alpha will go negative. A RESULT OF forgetting vertical shift
 
     error_prev = 0
     error_min = 100
@@ -129,16 +131,16 @@ if __name__=="__main__":
             grad_X = sol.y[2, :]
             grad_Z = sol.y[3, :]
        
-            CiF_f = sigmoid(CiF_f) 
+            CiF_f = sigmoid(CiF_f) - vs
             
             timeStep = timeVec[1] - timeVec[0]
             
             ga_L = np.sum(-2*(CI_Meas - CiF_f)*grad_Z*timeStep) 
             #print("gradient wrt alpha", ga_L)
             # step
-            rho = 5.5  # learning rate
+            rho = 1  # learning rate
             alpha = alpha - rho*ga_L
-            #gamma = gamma - rho*gL
+        
             
             print("alpha:", alpha)
 

alphaOnly_testing.py
@@ -79,11 +79,13 @@ if __name__=="__main__":
     kf, kr, alpha, gamma, L = paramValues(dset, CiF_0)
 
     #alpha = 1.14916717 # dset 1, requires gamma 1
-    alpha = 42.57296027 # dset 1, requires gamma 10
+    #alpha = 42.57296027 # dset 1, requires gamma 10
+    #alpha = 56.0113801 # good for dset 5
     #alpha = 8.6160731  # dset 3, requires gamma 1
     #alpha = 19.65693565 # dset 6, requires gamma 10
-    gamma = 10 # dsets 6, ... require gamma to be upped. 
+    gamma = 1 # dsets 6, ... require gamma to be upped. 
     #gamma = 1
+    alpha =  23.4181404221457
 
     kf = 0.1
     kr = 10
@@ -180,7 +182,7 @@ if __name__=="__main__":
     Ci_f = L - CiF_f
 
     # sigmoid calcium indicator so it is comoparable to calcium ion. 
-    CiF_f = sigmoid(CiF_f)
+    CiF_f = sigmoid(CiF_f) #- .15
 
     # transpose
     sol = np.transpose(mpc.data['_x'])